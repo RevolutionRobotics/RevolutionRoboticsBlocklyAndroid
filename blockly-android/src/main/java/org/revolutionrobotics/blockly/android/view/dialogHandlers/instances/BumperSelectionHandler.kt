9285527385f72a9029e1cd9edb817fcbd1905253
Balazs Maczak <maczak.balazs@gmail.com>
2020-04-29 16:01:40 +0200

    Updated Blockly, support for level 1 button block

--- blockly-android/src/main/java/org/revolutionrobotics/blockly/android/view/dialogHandlers/instances/BumperSelectionHandler.kt
+++ blockly-android/src/main/java/org/revolutionrobotics/blockly/android/view/dialogHandlers/instances/BumperSelectionHandler.kt
@@ -12,7 +12,8 @@ import org.revolutionrobotics.blockly.android.view.result.TextResult
 class BumperSelectionHandler  : JsPromptHandler {
 
     override fun canHandleRequest(message: String) = message == "block_bumper.name_input" ||
-            message == "block_wait_for_button_press.name_input"
+            message == "block_wait_for_button_press.name_input" ||
+            message == "block_on_button_pressed.name_input"
 
     override fun handleRequest(request: JSONObject, dialogFactory: DialogFactory, result: JsPromptResult) {
         dialogFactory.showBumperSelector(request.title(), request.subtitle(), request.defaultInput(),
